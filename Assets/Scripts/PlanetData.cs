2bb9639366e708692312f94e78b241a056136431
chanalvarez13 <chanalvarez13@gmail.com>
2026-07-15 16:57:57 -0700

    Added PlanetData Scriptable Object and PlanetManager for Monobehavior

---
+++ Assets/Scripts/PlanetData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Planet",menuName = "Space Incremental/Planet")]
+public class PlanetData : ScriptableObject
+{
+    public string planetName;
+    public double baseProduction;
+    public double baseCost;
+    public double worth;
+}
