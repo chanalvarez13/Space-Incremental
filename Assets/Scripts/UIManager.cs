0c0f4e38eb1a616a4fd81eec634ae2ef30d790cb
chanalvarez13 <chanalvarez13@gmail.com>
2026-07-15 22:08:32 -0700

    Energy and Production now display on the UI and is updated
    Energy is updated each frame
    Population is updated each frame (no population feature has been added yet...)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject PlanetUIMenu;
+    public PlanetManager planetTarget;
+    public Text energy;
+    public Text population;
 
     public void OnPlanetPress()
     {
         if (PlanetUIMenu != null)
         {
             PlanetUIMenu.SetActive(!PlanetUIMenu.activeSelf);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (planetTarget != null)
+        {
+            energy.text = planetTarget.Energy.ToString() + " Energy";
+            population.text = planetTarget.Population.ToString() + " Population";
+        }
+        else if (planetTarget == null)
+        {
+            Debug.Log("There is no target planet.");
         }
     }
 }
